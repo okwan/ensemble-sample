551bc3caa2e0f92492856a83b3e6fe5f92b3ccf9
Oscar Kwan <oscar.kwan@mailchimp.com>
2024-07-23 15:58:36 -0700

    clean up

--- src/components/SearchResults/index.js
+++ src/components/SearchResults/index.js
@@ -7,25 +7,29 @@ function SearchResults() {
   const { searchQuery, results, hasError } = useContext(SearchContext);
 
   return !hasError ? (
-    <ul className="movie-list">
-      {results.Search?.map((movie, index) => {
-        const { Poster: poster, Title: title, Year: year } = movie;
+    <React.Fragment>
+      <ul className="movie-list">
+        {results.Search?.map((movie, index) => {
+          const { Poster: poster, Title: title, Year: year } = movie;
 
-        return (
-          <li key={index} className="movie-list__item">
-            <img
-              src={poster}
-              alt={title + ' poster'}
-              className="movie-list__item-poster"
-            />
-            <p className="movie-list__item-title">{title}</p>
-            {year.endsWith('–') ? year.slice(0, year.length - 1) + ' – Current' : year}
-            <br/> <br />
-            <button onClick={() => {}}>Button</button>
-          </li>
-        );
-      })}
-    </ul>
+          return (
+            <li key={index} className="movie-list__item">
+              <img
+                src={poster}
+                alt={title + ' poster'}
+                className="movie-list__item-poster"
+              />
+              <p className="movie-list__item-title">{title}</p>
+              {year.endsWith('–')
+                ? year.slice(0, year.length - 1) + ' – Current'
+                : year}
+              <br /> <br />
+              <button onClick={() => {}}>Button</button>
+            </li>
+          );
+        })}
+      </ul>
+    </React.Fragment>
   ) : (
     searchQuery && (
       <p>
